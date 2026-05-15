a561bc52cb1a3c3b5555218d3a309c957639fdc5
yalshish <yousef.shaher@gmail.com>
2026-05-16 01:32:21 +0300

    Fixed Typo

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -15,8 +15,12 @@ namespace Portfolio.Data
         public DbSet<Skill> Skills { get; set; }
         public DbSet<TimelineEvent> TimelineEvents { get; set; }
         public DbSet<Credential> Credentials { get; set; }
-        public DbSet<PlatformProfile> PlatformProfile { get; set; }
+        public DbSet<PlatformProfile> PlatformProfiles { get; set; }
         public DbSet<ContactMessage> ContactMessages { get; set; }
+
+        // Explicit Many-to-Many Join Tables
+        public DbSet<ProjectSkill> ProjectSkills { get; set; }
+        public DbSet<CredentialSkill> CredentialSkills { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
